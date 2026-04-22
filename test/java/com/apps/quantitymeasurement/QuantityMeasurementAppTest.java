a697befe1697ae5b5f686df0252c5f4ff7029c5e
Joanna Jacob <joannajacob910@gmail.com>
2026-04-22 18:28:58 +0530

    first commit: UC1

---
+++ test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -0,0 +1,40 @@
+package com.apps.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testFeetEquality_SameValue() {
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(1.0);
+        assertTrue(feet1.equals(feet2), "1.0 ft should equal 1.0 ft");
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue() {
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(2.0);
+        assertFalse(feet1.equals(feet2), "1.0 ft should not equal 2.0 ft");
+    }
+
+    @Test
+    public void testFeetEquality_NullComparison() {
+        Feet feet1 = new Feet(1.0);
+        assertFalse(feet1.equals(null), "Feet value should not equal null");
+    }
+
+    @Test
+    public void testFeetEquality_DifferentClass() {
+        Feet feet1 = new Feet(1.0);
+        assertFalse(feet1.equals("1.0"), "Feet should not equal a non-numeric/different type");
+    }
+
+    @Test
+    public void testFeetEquality_SameReference() {
+        Feet feet1 = new Feet(1.0);
+        assertTrue(feet1.equals(feet1), "Feet object should equal itself (reflexive)");
+    }
+}
